a20b90410be3322f651c67dd2566e8db22211ea4
Matilde90 <matildealiffi@economist.com>
2020-05-16 23:13:18 +0100

    modified index.js with redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import './index.css';
 import 'tachyons';
-import {searchCountry} from './reducer'
+import { searchCountry } from './reducer';
 import App from './containers/App';
 
 const store = createStore(searchCountry);
 
-ReactDOM.render(<div>
-<App/>
-                </div>
-, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
